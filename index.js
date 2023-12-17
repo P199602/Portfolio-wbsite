c6107f62f2164bc9db9a8348e6c0975b0e93efc4
Pranshu kanthali <147531908+P199602@users.noreply.github.com>
2023-12-17 20:11:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+                         $(document).ready(function () { 
+                          setTimeout(function() { 
+                            $(".card").hide();  
+                           },1000);
+                        });
+                   
+                        $(document).ready(function () { 
+                            $("*").mouseenter(function() { 
+                              $(".card").show(2000);  
+                             });
+                            });
+                            
+                      $(document).ready(function(){
+                        setInterval( function(){
+                            $("#next").click();
+                          },2000 )
+                        })
+                        $(document).ready(function () { 
+                          $("#carouselExampleIndicators").mouseenter(function() { 
+                            $("h1").show(2000);  
+                           });
+                          });
+                          $(document).ready(function () { 
+                              $("h1").hide();  
+                             });
+                             $(document).ready(function () { 
+                              $("#aboutimg").hide();  
+                             });
+                             $(document).ready(function () { 
+                              $("*").mouseenter(function() { 
+                                $("#aboutimg").show(2000);  
+                               });
+                              });
+                        
+                              
